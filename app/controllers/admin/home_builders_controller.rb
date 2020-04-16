fd2fa6c8201d0cbd42b3019c4431d4224e4f4e7e
DarksideJP <darknebula1419@gmail.com>
2020-04-16 08:23:24 +0000

    [Add] admin_home_builder

--- app/controllers/admin/home_builders_controller.rb
+++ app/controllers/admin/home_builders_controller.rb
@@ -3,8 +3,41 @@ class Admin::HomeBuildersController < ApplicationController
   end
 
   def new
+  	@home_builder = HomeBuilder.new
+  end
+
+  def create
+  	@home_builder = HomeBuilder.new(home_builder_params)
+  	# binding.pry
+  	if @home_builder.save
+  		redirect_to admin_home_builders_path
+  	else
+  		reder :new
+  	end
+  end
+
+  def index
+  	@home_builders = HomeBuilder.all
   end
 
   def edit
+  	@home_builder = HomeBuilder.find(params[:id])
+  end
+
+  def update
+  	home_builder = HomeBuilder.find(params[:id])
+  	home_builder.update(home_builder_params)
+  	redirect_to admin_home_builders_path
+  end
+
+  def destroy
+  	home_builder = HomeBuilder.find(params[:id])
+  	home_builder.destroy
+  	redirect_to admin_home_builders_path
+  end
+
+  private
+  def home_builder_params
+  	params.require(:home_builder).permit(:name, :area)
   end
 end
