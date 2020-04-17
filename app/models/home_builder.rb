ae6644cc5fca6d128747b19283fac6ee86dc7019
DarksideJP <darknebula1419@gmail.com>
2020-04-17 06:13:36 +0000

    [Update] validation setting

--- app/models/home_builder.rb
+++ app/models/home_builder.rb
@@ -1,4 +1,6 @@
 class HomeBuilder < ApplicationRecord
+	validates :name, presence: true
+	validates :area, presence: true
 	has_many :contacts
 	enum area: {osaka: 1, kyoto: 2, nara: 3, hyogo: 4}
 end
