d4cc0a67862723811d6af064db8a0daaf028fd0c
DarksideJP <darknebula1419@gmail.com>
2020-04-18 09:55:52 +0000

    [Add] end_user_end_user

--- app/controllers/end_user/end_users_controller.rb
+++ app/controllers/end_user/end_users_controller.rb
@@ -1,7 +1,23 @@
 class EndUser::EndUsersController < ApplicationController
   def my_page
+  	@end_user = EndUser.find(current_end_user.id)
   end
 
   def edit
+  	# @end_user = EndUser.find(current_end_user.id)
+  end
+
+  def update
+       @end_user = EndUser.find(current_end_user.id)
+    if @end_user.update(end_user_params)
+       redirect_to my_page_path, notice: "登録情報を変更しました"
+    else
+       render :edit
+    end
+  end
+
+  private
+  def end_user_params
+  	params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :address, :phone_number, :is_unsubscribed, :email, :password, :password_confirmation)
   end
 end
