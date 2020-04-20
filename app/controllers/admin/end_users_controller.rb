ffa29c39b77aa2a783f1af1a36c076647b5d1e22
DarksideJP <darknebula1419@gmail.com>
2020-04-20 07:52:33 +0000

    [Add] end_user_unsubscribed

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,10 +1,26 @@
 class Admin::EndUsersController < ApplicationController
   def index
-  	@end_users = EndUser.all
+  	@end_users = EndUser.with_deleted
   end
 
   def show
-  	@end_user = EndUser.find(params[:id])
+  	@end_user = EndUser.with_deleted.find(params[:id])
   end
 
+  def update
+  	@end_user = EndUser.with_deleted.find(params[:id])
+  	if @end_user.update(end_user_params)
+  		if @end_user.is_unsubscribed == false
+  			@end_user.restore
+  		elsif @end_user.is_unsubscribed == true
+  			@end_user.destroy
+  		end
+  		redirect_to request.referer, notice: "退会ステータスを変更しました"
+  	end
+  end
+
+  private
+  def end_user_params
+  	params.require(:end_user).permit(:is_unsubscribed)
+  end
 end
