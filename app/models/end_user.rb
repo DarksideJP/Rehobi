e615f1b7ed3033952e4c3d7c5b114cb51a84a3e4
DarksideJP <darknebula1419@gmail.com>
2020-06-17 08:30:38 +0000

    [Add] name search

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -6,4 +6,11 @@ class EndUser < ApplicationRecord
          has_many :contacts
          has_many :favorites
          acts_as_paranoid
+  def self.search(search)
+    if search
+      where(['last_name || first_name LIKE ?', "%#{search}%"])
+    else
+      all
+    end
+  end
 end
